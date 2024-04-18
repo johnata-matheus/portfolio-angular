9cfda6b74f954b2f2e1db44790ff35d64a373056
Johnata Matheus <matheusabdala12@gmail.com>
2024-04-18 01:19:20 -0300

    feat:  create send-email and add toastr component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { TitleComponent } from '../title/title.component';
 import { PrimaryBtnComponent } from '../primary-btn/primary-btn.component';
+import { EmailService } from '../../services/email.service';
+import { IEmail } from '../../interfaces/IEmail';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [TitleComponent, PrimaryBtnComponent],
+  imports: [TitleComponent, PrimaryBtnComponent, FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  stateForm: boolean = false;
+
+  @ViewChild('form') emailForm!: NgForm;
+
+  constructor(private emailService: EmailService, private toastr: ToastrService){}
   
+  sendEmail(form: IEmail){
+    this.emailService.sendEmail(form).subscribe(
+      (res) => {
+        this.stateForm = true;
+        this.toastr.success("Mensagem enviada com sucesso!");
+        this.emailForm.resetForm();
+      },
+      (error) => {
+        this.toastr.error("Erro ao enviar a mensagem, tente novamente mais tarde.");
+        this.stateForm = false;
+      }
+    )
+  }
 }
